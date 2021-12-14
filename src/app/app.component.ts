5d0bef85fc8d691c82cef1d45aecd72f97390ac1
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2021-12-14 12:55:30 +0100

    Inplementacion de traductor para toda la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { LocalStorageService } from './services/local-storage.service';
 
@@ -8,40 +9,52 @@ import { LocalStorageService } from './services/local-storage.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  private langsAvailable=['es','en'];
-  constructor(private traductor:TranslateService, private storage:LocalStorageService) {
+  private langsAvailable = ['es', 'en'];
+  public subscription: any;
+  constructor(private traductor: TranslateService,
+    private storage: LocalStorageService,
+    private platform: Platform) {
+    this.platform.backButton.subscribeWithPriority(100, () => {
+      navigator['app'].exitApp();
+    });
 
     (async () => {
-       let lang = await storage.getItem("lang");
-       if (lang==null) {
-         lang=this.traductor.getBrowserLang();
-       } else {
-        lang=lang.lang;
-       }
-       if (this.langsAvailable.indexOf(lang)>-1) {
-         traductor.setDefaultLang(lang)
-       } else {
+      let lang = await storage.getItem("lang");
+      if (lang == null) {
+        lang = this.traductor.getBrowserLang();
+      } else {
+        lang = lang.lang;
+      }
+      if (this.langsAvailable.indexOf(lang) > -1) {
+        traductor.setDefaultLang(lang)
+      } else {
         traductor.setDefaultLang("en");
-       }
+      }
     })();
 
     //detectar el lenguaje del navegador
     //const lang = window.navigator.language.split("-")[0]
     const lang = this.traductor.getBrowserLang();
-    if (this.langsAvailable.indexOf(lang)>-1) {
+    if (this.langsAvailable.indexOf(lang) > -1) {
       traductor.setDefaultLang(lang);
     }
     traductor.setDefaultLang("en");
   }
-  
-  public async cambiaIdioma(event){
+  ionViewDidEnter() {
 
-    if(event && event.detail && event.detail.checked){
-      await this.storage.setItem('lang',{lang:'en'});
+  }
+
+
+  public async cambiaIdioma(event) {
+
+    if (event && event.detail && event.detail.checked) {
+      await this.storage.setItem('lang', { lang: 'en' });
       this.traductor.use('en');
-    }else{
-      await this.storage.setItem('lang',{lang:'es'});
+    } else {
+      await this.storage.setItem('lang', { lang: 'es' });
       this.traductor.use('es');
     }
   }
+
+
 }
